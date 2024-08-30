298de2e77944322ca95d0af55f61c03bd1289c35
adriano barbosa <adrianobarbosa_4@hotmail.com>
2024-08-30 17:09:49 -0300

    listando usuarios

--- src/main/java/adriano/barbosa/springsecurity/controller/TokenController.java
+++ src/main/java/adriano/barbosa/springsecurity/controller/TokenController.java
@@ -2,6 +2,7 @@ package adriano.barbosa.springsecurity.controller;
 
 import adriano.barbosa.springsecurity.dto.LoginRequest;
 import adriano.barbosa.springsecurity.dto.LoginResponse;
+import adriano.barbosa.springsecurity.entities.Role;
 import adriano.barbosa.springsecurity.repository.UserRepository;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.Instant;
+import java.util.stream.Collectors;
 
 @RestController
 public class TokenController {
@@ -44,11 +46,17 @@ public class TokenController {
        var now = Instant.now();
        var expiresIn = 300L;
 
+       var scopes = user.get().getRoles()
+               .stream()
+               .map(Role::getName)
+               .collect(Collectors.joining(""));
+
        var claims = JwtClaimsSet.builder()
                .issuer("mybackend")
                .subject(user.get().getUserId().toString())
                .issuedAt(now)
                .expiresAt(now.plusSeconds(expiresIn))
+               .claim("scope", scopes)
                .build();
 
        var jwtValue = jwtEncoder.encode(JwtEncoderParameters.from(claims)).getTokenValue();
